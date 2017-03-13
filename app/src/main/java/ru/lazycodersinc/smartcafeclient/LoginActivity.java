a3b05c410c80d6e2c8ad86cd2b384430d3f98fee
BobNobrain <nikita-ivanov.mail@yandex.ru>
2017-03-13 22:28:40 +0300

    Restored project and slightly changed login activity

--- app/src/main/java/ru/lazycodersinc/smartcafeclient/LoginActivity.java
+++ app/src/main/java/ru/lazycodersinc/smartcafeclient/LoginActivity.java
@@ -3,9 +3,6 @@ package ru.lazycodersinc.smartcafeclient;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
 
@@ -32,24 +29,16 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
-import static android.Manifest.permission.READ_CONTACTS;
-
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor>
+public class LoginActivity extends AppCompatActivity //implements LoaderCallbacks<Cursor>
 {
-
-	/**
-	 * Id to identity READ_CONTACTS permission request.
-	 */
-	private static final int REQUEST_READ_CONTACTS = 0;
-
 	/**
 	 * A dummy authentication store containing known user names and passwords.
 	 * TODO: remove after connecting to a real authentication system.
 	 */
-	private static final String[] DUMMY_CREDENTIALS = new String[]{"foo@example.com:hello", "bar@example.com:world"};
+	private static final String[] DUMMY_CREDENTIALS = new String[]{"IvanovNV:hello", "login:passw"};
 	/**
 	 * Keep track of the login task to ensure we can cancel it if requested.
 	 */
@@ -68,7 +57,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 		setContentView(R.layout.activity_login);
 		// Set up the login form.
 		mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-		populateAutoComplete();
+		// populateAutoComplete();
 
 		mPasswordView = (EditText) findViewById(R.id.password);
 		mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener()
@@ -98,62 +87,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 		mLoginFormView = findViewById(R.id.login_form);
 		mProgressView = findViewById(R.id.login_progress);
 	}
-
-	private void populateAutoComplete()
-	{
-		if (!mayRequestContacts())
-		{
-			return;
-		}
-
-		getLoaderManager().initLoader(0, null, this);
-	}
-
-	private boolean mayRequestContacts()
-	{
-		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
-		{
-			return true;
-		}
-		if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED)
-		{
-			return true;
-		}
-		if (shouldShowRequestPermissionRationale(READ_CONTACTS))
-		{
-			Snackbar.make(mEmailView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE).setAction(android.R.string.ok, new View.OnClickListener()
-
-			{
-				@Override
-				@TargetApi(Build.VERSION_CODES.M)
-				public void onClick(View v)
-				{
-					requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-				}
-			});
-		}
-		else
-		{
-			requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-		}
-		return false;
-	}
-
-	/**
-	 * Callback received when a permissions request has been completed.
-	 */
-	@Override
-	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
-	{
-		if (requestCode == REQUEST_READ_CONTACTS)
-		{
-			if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-			{
-				populateAutoComplete();
-			}
-		}
-	}
-
 
 	/**
 	 * Attempts to sign in or register the account specified by the login form.
@@ -193,7 +126,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 			focusView = mEmailView;
 			cancel = true;
 		}
-		else if (!isEmailValid(email))
+		else if (!isLoginValid(email))
 		{
 			mEmailView.setError(getString(R.string.error_invalid_email));
 			focusView = mEmailView;
@@ -216,10 +149,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 		}
 	}
 
-	private boolean isEmailValid(String email)
+	private boolean isLoginValid(String login)
 	{
 		//TODO: Replace this with your own logic
-		return email.contains("@");
+		return true;
 	}
 
 	private boolean isPasswordValid(String password)
@@ -271,56 +204,12 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 		}
 	}
 
-	@Override
-	public Loader<Cursor> onCreateLoader(int i, Bundle bundle)
-	{
-		return new CursorLoader(this,
-				// Retrieve data rows for the device user's 'profile' contact.
-				Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI, ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
-
-				// Select only email addresses.
-				ContactsContract.Contacts.Data.MIMETYPE + " = ?", new String[]{ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE},
-
-				// Show primary email addresses first. Note that there won't be
-				// a primary email address if the user hasn't specified one.
-				ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
-	}
-
-	@Override
-	public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor)
-	{
-		List<String> emails = new ArrayList<>();
-		cursor.moveToFirst();
-		while (!cursor.isAfterLast())
-		{
-			emails.add(cursor.getString(ProfileQuery.ADDRESS));
-			cursor.moveToNext();
-		}
-
-		addEmailsToAutoComplete(emails);
-	}
-
-	@Override
-	public void onLoaderReset(Loader<Cursor> cursorLoader)
-	{
-
-	}
-
-	private void addEmailsToAutoComplete(List<String> emailAddressCollection)
+	private void addLoginsToAutoComplete(List<String> emailAddressCollection)
 	{
 		//Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
 		ArrayAdapter<String> adapter = new ArrayAdapter<>(LoginActivity.this, android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
 
 		mEmailView.setAdapter(adapter);
-	}
-
-
-	private interface ProfileQuery
-	{
-		String[] PROJECTION = {ContactsContract.CommonDataKinds.Email.ADDRESS, ContactsContract.CommonDataKinds.Email.IS_PRIMARY,};
-
-		int ADDRESS = 0;
-		int IS_PRIMARY = 1;
 	}
 
 	/**
